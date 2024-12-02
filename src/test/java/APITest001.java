8a08c8bcba54a53ed15eb7800405afc39dc54836
nilesh.varhadi <varhadi.nilesh81@gmail.com>
2024-12-02 17:03:16 +0530

    27 project maven1111

---
+++ src/test/java/APITest001.java
@@ -0,0 +1,6 @@
+public class APITest001 {
+
+    public static void main(String[] args) {
+        System.out.println("API Testing");
+    }
+}
---
+++ src/test/java/APITest002.java
@@ -0,0 +1,24 @@
+import io.restassured.RestAssured;
+
+public class APITest002 {
+
+    public static void main(String[] args) {
+
+        // Gherkin syntax
+
+        // Full URL : https://restful-booker.herokuapp.com/booking/1
+        // base URI : https://restful-booker.herokuapp.com
+        // base path : /booking/1
+
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/1")
+                .when()
+                   .get()
+                .then().log().all()
+                   .statusCode(200);
+
+
+    }
+}
